e374bb29add063f10ea584707aeea147b05cf7a9
Sayantan Banerjee <sayantaninfinity16@gmail.com>
2020-12-26 00:29:16 +0530

    java wrapper class bug fixed

--- app/src/main/java/com/example/basicsoltest/SimpleContract.java
+++ app/src/main/java/com/example/basicsoltest/SimpleContract.java
@@ -10,17 +10,15 @@ import org.web3j.abi.datatypes.generated.Uint8;
 import org.web3j.crypto.Credentials;
 import org.web3j.protocol.Web3j;
 import org.web3j.protocol.core.RemoteCall;
-import org.web3j.protocol.core.RemoteFunctionCall;
 import org.web3j.protocol.core.methods.response.TransactionReceipt;
 import org.web3j.tx.Contract;
 import org.web3j.tx.TransactionManager;
-import org.web3j.tx.gas.ContractGasProvider;
 
 /**
  * <p>Auto generated code.
  * <p><strong>Do not modify!</strong>
  * <p>Please use the <a href="https://docs.web3j.io/command_line.html">web3j command line tools</a>,
- * or the org.web3j.codegen.SolidityFunctionWrapperGenerator in the 
+ * or the org.web3j.codegen.SolidityFunctionWrapperGenerator in the
  * <a href="https://github.com/web3j/web3j/tree/master/codegen">codegen module</a> to update.
  *
  * <p>Generated with web3j version 4.5.16.
@@ -40,37 +38,29 @@ public class SimpleContract extends Contract {
         super(BINARY, contractAddress, web3j, credentials, gasPrice, gasLimit);
     }
 
-    protected SimpleContract(String contractAddress, Web3j web3j, Credentials credentials, ContractGasProvider contractGasProvider) {
-        super(BINARY, contractAddress, web3j, credentials, contractGasProvider);
-    }
-
     @Deprecated
     protected SimpleContract(String contractAddress, Web3j web3j, TransactionManager transactionManager, BigInteger gasPrice, BigInteger gasLimit) {
         super(BINARY, contractAddress, web3j, transactionManager, gasPrice, gasLimit);
     }
 
-    protected SimpleContract(String contractAddress, Web3j web3j, TransactionManager transactionManager, ContractGasProvider contractGasProvider) {
-        super(BINARY, contractAddress, web3j, transactionManager, contractGasProvider);
-    }
-
-    public RemoteFunctionCall<BigInteger> get() {
-        final Function function = new Function(FUNC_GET, 
-                Arrays.<Type>asList(), 
+    public RemoteCall<BigInteger> get() {
+        final Function function = new Function(FUNC_GET,
+                Arrays.<Type>asList(),
                 Arrays.<TypeReference<?>>asList(new TypeReference<Uint8>() {}));
         return executeRemoteCallSingleValueReturn(function, BigInteger.class);
     }
 
-    public RemoteFunctionCall<BigInteger> integer() {
-        final Function function = new Function(FUNC_INTEGER, 
-                Arrays.<Type>asList(), 
+    public RemoteCall<BigInteger> integer() {
+        final Function function = new Function(FUNC_INTEGER,
+                Arrays.<Type>asList(),
                 Arrays.<TypeReference<?>>asList(new TypeReference<Uint8>() {}));
         return executeRemoteCallSingleValueReturn(function, BigInteger.class);
     }
 
-    public RemoteFunctionCall<TransactionReceipt> set(BigInteger _integer) {
+    public RemoteCall<TransactionReceipt> set(BigInteger _integer) {
         final Function function = new Function(
-                FUNC_SET, 
-                Arrays.<Type>asList(new org.web3j.abi.datatypes.generated.Uint8(_integer)), 
+                FUNC_SET,
+                Arrays.<Type>asList(new org.web3j.abi.datatypes.generated.Uint8(_integer)),
                 Collections.<TypeReference<?>>emptyList());
         return executeRemoteCallTransaction(function);
     }
@@ -83,22 +73,6 @@ public class SimpleContract extends Contract {
     @Deprecated
     public static SimpleContract load(String contractAddress, Web3j web3j, TransactionManager transactionManager, BigInteger gasPrice, BigInteger gasLimit) {
         return new SimpleContract(contractAddress, web3j, transactionManager, gasPrice, gasLimit);
-    }
-
-    public static SimpleContract load(String contractAddress, Web3j web3j, Credentials credentials, ContractGasProvider contractGasProvider) {
-        return new SimpleContract(contractAddress, web3j, credentials, contractGasProvider);
-    }
-
-    public static SimpleContract load(String contractAddress, Web3j web3j, TransactionManager transactionManager, ContractGasProvider contractGasProvider) {
-        return new SimpleContract(contractAddress, web3j, transactionManager, contractGasProvider);
-    }
-
-    public static RemoteCall<SimpleContract> deploy(Web3j web3j, Credentials credentials, ContractGasProvider contractGasProvider) {
-        return deployRemoteCall(SimpleContract.class, web3j, credentials, contractGasProvider, BINARY, "");
-    }
-
-    public static RemoteCall<SimpleContract> deploy(Web3j web3j, TransactionManager transactionManager, ContractGasProvider contractGasProvider) {
-        return deployRemoteCall(SimpleContract.class, web3j, transactionManager, contractGasProvider, BINARY, "");
     }
 
     @Deprecated
