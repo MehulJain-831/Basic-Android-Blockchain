aea3c5686e29b0120218d850368b7d00608cd580
Sayantan Banerjee <sayantaninfinity16@gmail.com>
2020-12-26 10:23:46 +0530

    code completion of reading and writing data into contract

--- app/src/main/java/com/example/basicsoltest/MainActivity.kt
+++ app/src/main/java/com/example/basicsoltest/MainActivity.kt
@@ -2,11 +2,74 @@ package com.example.basicsoltest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.web3j.crypto.Credentials
+import org.web3j.protocol.Web3jFactory
+import org.web3j.protocol.core.methods.response.TransactionReceipt
+import org.web3j.protocol.infura.InfuraHttpService
+import java.math.BigInteger
+import java.util.concurrent.Future
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var sendButton : Button
+    private lateinit var dispTextView: TextView
+    private lateinit var editTextNumber : EditText
+    private lateinit var simpleContract: SimpleContract
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        sendButton = findViewById(R.id.send)
+        dispTextView = findViewById(R.id.dispValue)
+        editTextNumber = findViewById(R.id.editTextNumber)
+
+        val contractAddress = "CONTRACT_ADDRESS"
+        val url = "URL_COMPLETE_ADDRESS"
+        val web3j = Web3jFactory.build(InfuraHttpService(url))
+        val gasLimit: BigInteger = BigInteger.valueOf(20_000_000_000L) //change this as required
+        val gasPrice: BigInteger = BigInteger.valueOf(4300000) // this value also
+        val credentials = Credentials.create("YOUR_PRIVATE_KEY")
+
+        simpleContract  = SimpleContract.load(contractAddress, web3j, credentials, gasLimit, gasPrice)
+
+        sendButton.setOnClickListener {
+            val string : String = editTextNumber.text.toString()
+            if(string != ""){
+                GlobalScope.launch(Dispatchers.Main) {
+                    sendAndRetrieveData(string.toBigInteger())
+                }
+            }
+
+        }
+    }
+
+    private suspend fun sendAndRetrieveData(num : BigInteger) {
+        val job = GlobalScope.launch(Dispatchers.Default) {
+            // write to contract
+            val transactionReceipt: Future<TransactionReceipt>? = simpleContract.set(num).sendAsync()
+            val result = "Successful transaction. Gas used: ${transactionReceipt?.get()?.blockNumber}  ${transactionReceipt?.get()?.gasUsed}"
+            Log.i("##SUCCESS_WRITE_DATA##", result)
+
+            // read from contract
+            val getValue: Future<BigInteger>? = simpleContract.get().sendAsync()
+            val convertToString: BigInteger? = getValue?.get()
+            Log.i("##SUCCESS_READ_DATA##", "contract value returned: $convertToString")
+
+            // returned value displayed to text view in Main Thread
+            withContext(Dispatchers.Main){
+                dispTextView.text = convertToString.toString()
+            }
+        }
+        job.join()
     }
 }
